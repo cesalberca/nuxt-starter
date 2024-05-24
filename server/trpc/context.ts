ea484b4c83771b379b3a991284315b4800d96e45
cesalberca <cesalberca@gmail.com>
2024-05-24 15:22:15 +0200

    Add middlewarews

--- server/trpc/context.ts
+++ server/trpc/context.ts
@@ -1,8 +1,12 @@
-import type { inferAsyncReturnType } from "@trpc/server";
-import type { H3Event } from "h3";
+import type {inferAsyncReturnType} from "@trpc/server";
+import type {H3Event} from "h3";
+import {enhance} from "@zenstackhq/runtime";
+import {prisma} from "~/server/utils/prisma";
 
 export const createContext = async (event: H3Event) => {
-  return { user: event.context.user };
+    return {
+        user: event.context.user, prisma: await enhance(prisma),
+    };
 };
 
 export type Context = inferAsyncReturnType<typeof createContext>;
