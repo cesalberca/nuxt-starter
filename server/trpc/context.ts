ba8417364bd519ac3eb8e3288a228d279ff1416b
cesalberca <cesalberca@gmail.com>
2024-05-27 16:34:09 +0200

    Add check

--- server/trpc/context.ts
+++ server/trpc/context.ts
@@ -1,12 +1,23 @@
-import type {inferAsyncReturnType} from "@trpc/server";
-import type {H3Event} from "h3";
-import {enhance} from "@zenstackhq/runtime";
-import {prisma} from "~/server/utils/prisma";
+import type { inferAsyncReturnType } from "@trpc/server";
+import type { H3Event } from "h3";
+import { enhance } from "@zenstackhq/runtime";
+
+import { prisma } from "~/server/utils/prisma";
 
 export const createContext = async (event: H3Event) => {
-    return {
-        user: event.context.user, prisma: await enhance(prisma),
-    };
+  return {
+    user: event.context.user,
+    ...(event.context.user
+      ? {
+          prisma: enhance(prisma, {
+            user: {
+              ...event.context.user,
+              id: event.context.user?.id,
+            },
+          }),
+        }
+      : {}),
+  };
 };
 
 export type Context = inferAsyncReturnType<typeof createContext>;
