7f14ebd495ff3b4b94bfa2b0e160f3bf5df48c61
cesalberca <cesalberca@gmail.com>
2024-05-27 16:49:14 +0200

    Integrate Zenstack

--- server/trpc/routers/user.ts
+++ server/trpc/routers/user.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
 import { protectedProcedure, publicProcedure, router } from "~/server/trpc/trpc";
-import { prisma } from "~/server/utils/prisma";
 
 export const userRouter = router({
   get: publicProcedure.query(({ ctx }) => {
@@ -14,7 +13,7 @@ export const userRouter = router({
       }),
     )
     .mutation(({ input, ctx }) => {
-      return prisma.user.update({
+      return ctx.prisma.user.update({
         where: {
           id: ctx.user.id,
         },
--- server/trpc/trpc.ts
+++ server/trpc/trpc.ts
@@ -1,7 +1,7 @@
-import {initTRPC, TRPCError} from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import superjson from "superjson";
-import type {Context} from "~/server/trpc/context";
 
+import type { Context } from "~/server/trpc/context";
 
 const t = initTRPC.context<Context>().create({
   transformer: superjson,
@@ -11,8 +11,13 @@ const authMiddleware = t.middleware(({ ctx, next }) => {
   if (!ctx.user) {
     throw new TRPCError({ code: "UNAUTHORIZED" });
   }
+
+  if (!ctx.prisma) {
+    throw new TRPCError({ code: "NOT_FOUND" });
+  }
+
   return next({
-    ctx: { ...ctx, user: ctx.user },
+    ctx: { ...ctx, user: ctx.user, prisma: ctx.prisma },
   });
 });
 
